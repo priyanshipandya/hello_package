77607721f7d5fe0f282da3044213a00e491addd0
Priyanshi Pandya <priyanshi.p@simformsolutions.com>
2023-07-03 15:56:30 +0530

    Initial commit

---
+++ lib/hello_package.dart
@@ -0,0 +1,16 @@
+library cnt_package;
+
+import 'package:flutter/material.dart';
+
+class Counter extends StatelessWidget {
+  const Counter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Heelo"),
+      ),
+    );
+  }
+}
